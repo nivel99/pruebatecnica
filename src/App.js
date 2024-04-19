0fca60fda623cec9a7cd4a8f1ca9d1e8af7ef241
nivel99 <inteligenciafuturatv@gmail.com>
2024-04-19 09:14:16 -0500

    Sliders con las capas listas y funcionando bien

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { ComparisonSlider } from 'react-comparison-slider';
+
+function App() {
+
+
+  return (
+    <ComparisonSlider
+      defaultValue={50}
+      itemOne={<div className="bg-red-200">
+        <div className='logo1'></div>
+        <div className='blueCar'></div>
+      </div>}
+      itemTwo={<div className="bg-blue-200">
+        <div className='logo2'></div>
+        <div className='whiteCar'></div>
+      </div>}
+      aspectRatio={16 / 9}
+      orientation="horizontal"
+    />
+  );
+}
+
+export default App;
